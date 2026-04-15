61f4c9f5e632278c75cd201cc9bc9916f65463b1
kqngyn <130106632+kqngyn@users.noreply.github.com>
2026-04-15 13:00:27 -0700

    Initial Commit

---
+++ AudioVisual/AudioVisualApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioVisualApp.swift
+//  AudioVisual
+//
+//  Created by iya student on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct AudioVisualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
